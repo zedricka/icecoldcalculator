9fe3218a8f1e1ded1704b9205a7bac15c68416ef
zedricka <zedrick.ara@gmail.com>
2016-11-21 08:32:34 -1000

    wow

--- calculator.js
+++ calculator.js
@@ -1,6 +1,21 @@
 var justKidding ;
 
 function rawrxd (D, E, S){
-  if (D =)
-  console.log ()
+  if (D == "add" || D == "addition"){
+    justKidding = E + S;
+  console.log (justKidding);
+  }
+  else if (D == "subtract" || D == "subtraction"){
+    justKidding = E - S;
+  console.log (justKidding);    
+  }
+  else if (D == "multiply" || D == "multiplication"){
+    justKidding = E * S;
+  console.log (justKidding);  
+  }
+    else if (D == "divide" || D == "division"){
+    justKidding = E / S;
+  console.log (justKidding); 
+  }
 }
+rawrxd ("divide", 420, 74);
